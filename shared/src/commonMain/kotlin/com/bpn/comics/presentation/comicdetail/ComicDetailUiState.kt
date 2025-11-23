a49ff5ce4d1ab5f1980fe0b1a11de98836da211b
Pardip235 <pardip235@gmail.com>
2025-11-23 08:38:17 +0100

    docs: add comprehensive KDoc comments
    - Add KDoc to all ViewModels with class and method documentation
    - Document all Use Cases with purpose and parameters
    - Add property descriptions to UI state classes
    - Enhance Repository interface documentation
    - Add @param and @return tags where applicable
    - Improve API interface documentation

--- shared/src/commonMain/kotlin/com/bpn/comics/presentation/comicdetail/ComicDetailUiState.kt
+++ shared/src/commonMain/kotlin/com/bpn/comics/presentation/comicdetail/ComicDetailUiState.kt
@@ -1,10 +1,14 @@
 package com.bpn.comics.presentation.comicdetail
 
-import com.bpn.comics.data.model.Comic
+import com.bpn.comics.domain.model.Comic
 import com.bpn.comics.util.ErrorType
 
 /**
  * UI state for the comic detail screen.
+ * 
+ * @property comic The comic to display, or null if not loaded
+ * @property isLoading Whether the comic is being loaded
+ * @property errorType The current error, if any
  */
 data class ComicDetailUiState(
     val comic: Comic? = null,
--- shared/src/commonMain/kotlin/com/bpn/comics/presentation/favorites/FavoritesUiState.kt
+++ shared/src/commonMain/kotlin/com/bpn/comics/presentation/favorites/FavoritesUiState.kt
@@ -1,10 +1,14 @@
 package com.bpn.comics.presentation.favorites
 
-import com.bpn.comics.data.model.Comic
+import com.bpn.comics.domain.model.Comic
 import com.bpn.comics.util.ErrorType
 
 /**
  * UI state for the favorites screen.
+ * 
+ * @property favorites The list of favorite comics to display
+ * @property isLoading Whether favorites are being loaded
+ * @property errorType The current error, if any
  */
 data class FavoritesUiState(
     val favorites: List<Comic> = emptyList(),
